284383b2c5c490ce8d5aff4653546f89df054761
truong <htr.lee@gmail.com>
2016-12-31 11:38:46 +0700

    update authService

--- src/app/shared/security/auth.service.ts
+++ src/app/shared/security/auth.service.ts
@@ -1,28 +1,37 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {FirebaseAuth} from "angularfire2";
-import {Observable, Subject} from "rxjs";
+import {Observable, Subject, BehaviorSubject} from "rxjs";
+import {AuthInfo} from "./auth-info";
 
 @Injectable()
 export class AuthService {
 
-  constructor(private  auth: FirebaseAuth) { }
+    static UNKNOWN_USER = new AuthInfo(null);
 
-  login(email,password):Observable<any>{
-   return this.fromFirebaseAuthPromise(this.auth.login({email,password}));
-  }
+    authInfo$: BehaviorSubject<AuthInfo> = new BehaviorSubject<AuthInfo>(AuthService.UNKNOWN_USER);
 
-  fromFirebaseAuthPromise(promise):Observable<any>{
+    constructor(private  auth: FirebaseAuth) {
+    }
 
-    const subject = new Subject<any>();
-    promise.then(res => {
-      subject.next(res);
-      subject.complete();
-    },
-    err => {
-      subject.error(err);
-      subject.complete();
-    });
-    return subject.asObservable();
-  }
+    login(email, password): Observable<any> {
+        return this.fromFirebaseAuthPromise(this.auth.login({email, password}));
+    }
+
+    fromFirebaseAuthPromise(promise): Observable<any> {
+
+        const subject = new Subject<any>();
+        promise.then(res => {
+                const authInfo = new AuthInfo(this.auth.getAuth().uid);
+                this.authInfo$.next(authInfo);
+                subject.next(res);
+                subject.complete();
+            },
+            err => {
+                this.authInfo$.error(err);
+                subject.error(err);
+                subject.complete();
+            });
+        return subject.asObservable();
+    }
 
 }
